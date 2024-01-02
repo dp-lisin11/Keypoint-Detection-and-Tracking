a3bc4939c433649251b227c947447dd113e21727
dp-lisin11 <b220023@iiit-bh.ac.in>
2024-01-02 22:24:31 +0530

    changed directory

---
+++ lib/camera_streamer.py
@@ -0,0 +1,48 @@
+import cv2
+import time
+
+class CameraStreamer:
+    def __init__(self, source=0):
+        self.source = source
+        self.cap = cv2.VideoCapture(source)   
+        self.start_time = time.time()
+        self.frame_count = 0
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            print("Failed to grab frame")
+        return frame
+
+    def start_streaming(self):
+        while True:
+            frame = self.get_frame()
+            if frame is None:
+                break
+
+            # Calculate FPS
+            self.frame_count += 1
+            elapsed_time = time.time() - self.start_time
+            fps = self.frame_count / elapsed_time
+
+            # Display FPS on the frame
+            cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            # Display the frame
+            cv2.imshow("Camera Stream", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def release_camera(self):
+        self.cap.release()
+
+
+if __name__ == "__main__":
+    camera_streamer = CameraStreamer()  
+    camera_streamer.start_streaming()
+
+    # camera_streamer.release_camera()
---
+++ lib/yolov8_model_loader.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+import cv2
+
+class YOLOv8ModelLoader:
+    def __init__(self, model_path='yolov8m.pt'):
+        self.model = YOLO(model_path)
+
+    def detect_objects(self, frame):
+        # Run YOLOv8 object detection on the frame
+        detections = self.model.predict(frame)
+
+        # Process detections and extract relevant information
+        objects = []
+        for detection in detections:
+            try:
+                box=detection[0].boxes
+                bbox=detection[0].boxes.xyxy
+                confidence=detection[0].boxes.conf.item()
+                class_id = detection[0].names[box.cls[0].item()]
+                
+                # Append the detected object information to the result list
+                objects.append({
+                    'class': class_id,
+                    'confidence': confidence,
+                    'bbox': bbox.tolist()  # Convert NumPy array to list
+                })
+            except Exception as e:
+                print(e)
+            
+            #print(detection)
+        return objects
