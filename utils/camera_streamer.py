3dca8662947f9d3174c937acc952ce3ebe3a9c21
dp-lisin11 <b220023@iiit-bh.ac.in>
2024-01-07 13:44:26 +0530

    minor fixes

---
+++ utils/camera_streamer.py
@@ -0,0 +1,80 @@
+import cv2
+from ultralytics import YOLO
+import mediapipe as mp
+from keypoint_detection_model_loader import KeypointDetectionModelLoader
+
+#from Pose_detection import PoseDetectionModelLoader
+
+keypoint_detector = KeypointDetectionModelLoader()
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+
+class CameraStreamer:
+    def __init__(self, source=0):
+        self.source = source
+        self.cap = cv2.VideoCapture(source)   
+        self.model = YOLO('yolov8m.pt')
+        #self.pose_loader=PoseDetectionModelLoader()
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            print("Failed to grab frame")
+        return frame
+
+    def start_streaming(self):
+        while True:
+            frame = self.get_frame()
+            if frame is None:
+                break            
+
+            """
+            The following code is to open object detection window
+            """
+            self.model(source=frame, show=True, conf=0.4, save=False)
+            
+            """
+            The following code is to view landmarks in the camera stream
+            """
+            results = mp_pose.Pose(static_image_mode=True).process(frame)   
+            circle_radius = int(.007 * frame.shape[1])
+            # Specifies the drawing style for the 'landmarks'.
+            point_spec = mp_drawing.DrawingSpec(color=(220, 100, 0), thickness=-1, circle_radius=circle_radius)
+            # Draws the 'landmarks' on the image.
+            mp_drawing.draw_landmarks(frame,
+                              landmark_list=results.pose_landmarks,
+                              landmark_drawing_spec=point_spec)       
+           
+            keypoints = keypoint_detector.detect_keypoints(frame)
+            if keypoints is not None:
+                mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+             
+            """
+            The following code is to check if 
+            the given coordinates for virtual line is crossed or not
+            
+            *** change line_start and line_end as per requirement  
+            """
+            
+            if keypoint_detector.has_crossed_line(frame,line_end=(640, 200),line_start=(0, 200)) and results.pose_landmarks:
+                cv2.line(frame, (0, 200), (640, 200), (0, 0, 255), 2)
+                print("Line crossed")
+            cv2.line(frame, (0, 200), (640, 200), (0, 255, 0), 2)
+        
+            
+           # Display the frame
+            cv2.imshow('Keypoints and Line Crossing', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def release_camera(self):
+        self.cap.release()
+
+
+if __name__ == "__main__":
+    camera_streamer = CameraStreamer()  
+    camera_streamer.start_streaming()
+    camera_streamer.release_camera()
---
+++ utils/keypoint_detection_model_loader.py
@@ -0,0 +1,60 @@
+import cv2
+import mediapipe as mp
+import json
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+class KeypointDetectionModelLoader:
+    def __init__(self):
+        self.mp_pose = mp_pose.Pose()
+        self.results = None
+
+    def detect_keypoints(self, frame):
+        # Convert the frame to RGB for Mediapipe
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Run human body keypoint detection and tracking using MediaPipe
+        self.results = self.mp_pose.process(rgb_frame)
+
+        # Process keypoints and extract relevant information
+        landmarks_info = []
+
+        if self.results.pose_landmarks:
+            for i, landmark in enumerate(self.results.pose_landmarks.landmark):
+                landmarks_info.append({
+                    "Landmark #{}".format(i): {
+                        "x": landmark.x,
+                        "y": landmark.y,
+                        "z": landmark.z,
+                        "visibility": landmark.visibility,
+                        "presence": landmark.presence
+                    }
+                })
+        print(landmarks_info)
+        return {"Landmarks": landmarks_info}
+    
+    
+    """
+    This function returns True if a person has crossed the line, False otherwise.
+    line_start and line_end: coordinates can be changed as per requirements
+    """
+    def has_crossed_line(self, frame, line_end, line_start):
+    # Get the coordinates of the detected pose landmarks
+        img=frame.copy()
+        results = mp_pose.Pose(static_image_mode=False).process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        landmarks = results.pose_landmarks.landmark
+        
+    # Choose a suitable keypoint to track (foot)
+        rfoot_x = int(landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x * img.shape[1])
+        rfoot_y = int(landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].y * img.shape[0])
+        lfoot_x = int(landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].x * img.shape[1])
+        lfoot_y = int(landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].y * img.shape[0])
+
+    # Check if the foot is below the line and has crossed it horizontally
+        if (rfoot_y > line_end[1] and rfoot_x > line_start[0] and rfoot_x < line_end[0]) or (lfoot_y > line_end[1] and lfoot_x > line_start[0] and lfoot_x < line_end[0]):
+            return True
+        else:
+            return False
+        
+
